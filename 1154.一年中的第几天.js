04ce8523b217996535cfa29e8cf6c0b8f237bfae
lhf6623 <1336955219@qq.com>
2024-09-13 18:43:45 +0800

    add: [1154] 一年中的第几天

---
+++ 1154.一年中的第几天.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1154 lang=javascript
+ *
+ * [1154] 一年中的第几天
+ */
+
+// @lc code=start
+/**
+ * @param {string} date
+ * @return {number}
+ */
+var dayOfYear = function (date) {
+  let [year, month, day] = date.split('-').map(Number)
+  let res = 0
+  let arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    res += (month <= 2 ? 0 : 1)
+  }
+  for (let i = 0; i < month - 1; i++) {
+    res += arr[i]
+  }
+  return res + day
+  // 暴力
+  // return (Date.parse(date) - Date.parse(`${year - 1}-12-31`)) / 1000 / 60 / 60 / 24
+};
+// @lc code=end
+
